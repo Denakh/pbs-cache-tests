35ac5505e3e493b4d90d53435ef31f05d530ce2b
dkhokhlin <denakhokh@gmail.com>
2020-12-19 23:52:00 +0200

    Completed small model changes

--- src/test/groovy/com/githab/denakh/cache/functional/system/model/SavedCreativesInfo.groovy
+++ src/test/groovy/com/githab/denakh/cache/functional/system/model/SavedCreativesInfo.groovy
@@ -8,6 +8,10 @@ class SavedCreativesInfo {
     List<SavedCreativeInfo> savedCreativeInfoList
 
     SavedCreativesInfo(Cache cache, CachePostResponse cachePostResponse) {
+        if (cache.puts.empty || cache.puts.size() != cachePostResponse.responses.size()) {
+            throw new IllegalArgumentException("Cache puts list size and cache post responses list size aren't equal " +
+                    "or some of the lists is empty: \nCache puts:${cache.puts} \nCache post responses: ${cachePostResponse.responses}")
+        }
         savedCreativeInfoList = []
         // Indexes of creatives in cache and their UUID indexes in response are actually the same, so we can do:
         for (i in 0..<cache.puts.size()) {
